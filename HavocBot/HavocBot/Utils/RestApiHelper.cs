1017e82efb83f802a0862457323ec4fa0fa2863b
Tomi Paananen <tomi.paananen@microsoft.com>
2018-03-06 15:41:20 +0000

    Adds RestApiHelper class.

---
+++ HavocBot/HavocBot/Utils/RestApiHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HavocBot.Utils
+{
+    public class RestApiHelper
+    {
+        /// <summary>
+        /// Executes an HTTP GET operation using the given URI.
+        /// </summary>
+        /// <param name="uri">The request URI.</param>
+        /// <returns>The response as string or null in case of a failure.</returns>
+        public static async Task<string> ExecuteHttpGet(string uri)
+        {
+            if (string.IsNullOrEmpty(uri))
+            {
+                throw new ArgumentNullException("URI cannot be empty");
+            }
+
+            string requestUri = Uri.EscapeDataString(uri);
+            string response = null;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage httpResponseMessage = null;
+
+                try
+                {
+                    httpResponseMessage = await httpClient.GetAsync(requestUri);
+
+                    if (httpResponseMessage.IsSuccessStatusCode)
+                    {
+                        response = await httpResponseMessage.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Failed to get response - status code was: {httpResponseMessage.StatusCode}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Failed to get response: {e.Message}");
+                }
+            }
+
+            return response;
+        }
+    }
+}
